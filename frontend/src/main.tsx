d42743bb70f72c658b5174b2164ed4e2fc691f26
Darrin Stogsdill <215324173+dstogsdill1@users.noreply.github.com>
2026-07-18 11:35:03 -0500

    Load source-normalized parity styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/app.css';
 import './styles/sheet.css';
+import './styles/sourceParity.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
