bf6342982aa9f7abc3c0f34554374159cd600297
Darrin Stogsdill <215324173+dstogsdill1@users.noreply.github.com>
2026-07-23 07:28:56 -0500

    Keep print export free of editor navigation controls

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './clipboardBridge';
+import App from './App';
 import HomeApp from './HomeApp';
 import EditorEntry from './EditorEntry';
 import './styles/app.css';
@@ -13,9 +14,8 @@ import './styles/home.css';
 
 function Root() {
   const params = new URLSearchParams(window.location.search);
-  const editorMode = params.get('editor') === '1'
-    || params.get('print') === '1'
-    || params.get('help') === '1';
+  if (params.get('print') === '1') return <App />;
+  const editorMode = params.get('editor') === '1' || params.get('help') === '1';
   return editorMode ? <EditorEntry /> : <HomeApp />;
 }
 
