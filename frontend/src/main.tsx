fc81176c9755e078a3040c01be8efd327e7baf9f
Darrin Stogsdill <215324173+dstogsdill1@users.noreply.github.com>
2026-07-23 07:26:44 -0500

    Route synced project home separately from page editor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './clipboardBridge';
-import App from './App';
+import HomeApp from './HomeApp';
+import EditorEntry from './EditorEntry';
 import './styles/app.css';
 import './styles/sheet.css';
 import './styles/sourceParity.css';
 import './styles/symbolMapper.css';
 import './styles/textBoxFormatting.css';
 import './styles/statusHelp.css';
+import './styles/home.css';
+
+function Root() {
+  const params = new URLSearchParams(window.location.search);
+  const editorMode = params.get('editor') === '1'
+    || params.get('print') === '1'
+    || params.get('help') === '1';
+  return editorMode ? <EditorEntry /> : <HomeApp />;
+}
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
