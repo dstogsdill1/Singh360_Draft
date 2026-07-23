876464d5200058b5bf9f5b7552ebc2bd29710234
Darrin Stogsdill <215324173+dstogsdill1@users.noreply.github.com>
2026-07-23 06:13:33 -0500

    Route app landing to project home before editor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './clipboardBridge';
 import App from './App';
+import HomeApp from './HomeApp';
 import './styles/app.css';
 import './styles/sheet.css';
 import './styles/sourceParity.css';
+import './styles/home.css';
+
+function Root() {
+  const params = new URLSearchParams(window.location.search);
+  const editorMode = params.get('editor') === '1' || params.get('print') === '1';
+  return editorMode ? <App /> : <HomeApp />;
+}
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
